fb18028cee79aa994e29f0ed4b45f08925309b5e
Albert Li <li.albert365@gmail.com>
2025-01-13 16:21:25 -0500

    feat: Add additional functionality to Canvas
    - Added additional keyboard and mouse functionality, such as ability to select node by clicking, and edit the text within.

--- src/classes/Rectangle.ts
+++ src/classes/Rectangle.ts
@@ -1,4 +1,14 @@
-import { shapes } from '@joint/core'
+import { shapes, util } from '@joint/core'
+
+// Append a foreignObject to the default Rectangle SVG containing an input.
+// Input should allow the text of the node to become editable when the element is double clicked.
+// Not visible by default.
+const markup = util.svg/*xml*/ `
+<foreignObject @selector="foreignObject">
+  <div @selector="inputWrapper">
+    <input @selector="rectangleInput" class="rectangle-input" type="text"/>
+  </div>
+</foreignObject>`
 
 /**
  * Class representing a Rectangle or Node on the canvas.
@@ -6,6 +16,12 @@ import { shapes } from '@joint/core'
  * such as fixed height, width, and colour.
  */
 export class Rectangle extends shapes.standard.Rectangle {
+  preinitialize() {
+    super.preinitialize()
+
+    this.markup = this.markup.concat(markup)
+  }
+
   /**
    *
    * @param args - JointJS RectangleAttributes object.
@@ -16,9 +32,31 @@ export class Rectangle extends shapes.standard.Rectangle {
       size: { width: 150, height: 65 },
       attrs: {
         body: { fill: 'rgb(191, 177, 223)', rx: 5, ry: 5 },
+        foreignObject: {
+          x: 0,
+          y: 0,
+          width: 'calc(w)',
+          height: 'calc(h)',
+          visibility: 'hidden',
+        },
+        inputWrapper: {
+          style: {
+            display: 'flex',
+            'justify-content': 'Center',
+            'align-items': 'Center',
+            height: '100%',
+          },
+        },
         ...attrs,
       },
       ...rest,
     })
   }
+
+  defaults() {
+    return { ...super.defaults, type: 'flowCreate.rectangle' }
+  }
+
+  // Want to customize the SVG for the JointJS Rectangle shape so that
+  // it includes an input that becomes editable when we double click the element.
 }
---
+++ src/classes/RectangleView.ts
@@ -0,0 +1,3 @@
+import { dia } from '@joint/core'
+
+export class RectangleView extends dia.ElementView {}
